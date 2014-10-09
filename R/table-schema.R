02a396e777242fac23177a1bdede5bb4005ffa72
Edwin de Jonge <edwindjonge@gmail.com>
2014-10-09 09:50:46 +0200

    wip

--- R/table-schema.R
+++ R/table-schema.R
@@ -22,8 +22,9 @@ derive_schema <- function(tbl, ...){
            integer   = "integer",
            character = "string",
            factor    = "string",
-           Date      = "datetime",
-           "Any"
+           Date      = "date",
+           POSIXct   = "datetime",
+           "any"
     )
   })
   fields <- data.frame(name=name, title=name, type=type)
@@ -35,4 +36,33 @@ read_schema <- function(file, ...){
   obj <- jsonlite::fromJSON(file)
   #TODO check obj
   table_schema(obj)
+}
+
+#' Checks if data.frame x conforms to schema
+# TODO allow for number/integer and date/datetime conflicts? : option
+# Should order of the variables be identical? : yes
+#' @export
+check_schema <- function(tbl, schema, ...){
+  name <- dplyr::tbl_vars(tbl)
+  check_name <- name == schema$field$name
+  if (!all(check_name)){
+    stop("Name conflict: ", name, " and ", schema$field$name, "are not equal")
+  }
+  
+  tbl_schema <- derive_schema(tbl)
+  invisible(TRUE)
+}
+
+# data may have more columns, so reorder and make a selection that follows the 
+# schema
+#' @export
+select_with_schema <- function(tbl, schema, ...){
+  #TODO may generate error!
+  vars <- schema$fields$name
+  if (!all(vars %in% dplyr::tbl_vars(tbl))){
+    #TODO improve error message
+    stop("Data does not contain all variables: ", call. = FALSE)
+  }
+  #TODO check schema for type?
+  dplyr::select_(tbl, .dots=vars)
 }
